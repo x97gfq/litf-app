25a5188a0c5bf2759d0e49fb379052460c37bfd4
Jamie Symonds <x97gfq@hotmail.com>
2020-12-04 19:31:49 -0400

    .

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="about">
+    <h3>About</h3>
     <br/>
     <br/>
     <br/>
     <br/>
     <br/>
-    <br/>
+    <p><router-link to="/">Back to Intro</router-link></p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  color: white;
+  padding-top: 20px;
+}
+</style>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
