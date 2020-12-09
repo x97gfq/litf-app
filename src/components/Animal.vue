6ced4a51ee677bafc6fbf478e3f10ec27b43b84e
Jamie Symonds <x97gfq@hotmail.com>
2020-12-09 19:10:40 -0400

    animal image display

--- src/components/Animal.vue
+++ src/components/Animal.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="animal">
-        <img class="animal_image" 
+        <img @load="onImgLoad"
         v-bind:src="require('../assets/animals/' + image)"
         v-bind:alt="animal_name" 
-        v-bind:title="animal_name"/>
+        v-bind:title="animal_name" 
+        id="animal_reveal" style="display: none;"/>
 
-      <audio autoplay>
+      <audio autoplay style="display: none;">
         <source v-bind:src="require('../assets/audio/' + sound)" type="audio/ogg">
       </audio>
     </div>
@@ -14,23 +15,30 @@
 <style scoped>
 .animal {
     background-color: black;
-    color: yellow;
-    height: 100%;
-}
-.animal_image {
-    width: 90%;
-    height: auto;
-    margin: auto;
+    color: rgb(251,288,64);
 }
 </style>
 
 <script>
+import $ from 'jquery'
+
 export default {
   name: 'Animal',
   props: {
       animal_name: String,
       image: String,
       sound: String
-  }
+  },
+  methods: {
+    onImgLoad () {
+      //this method is fired when the image has finished loading
+      console.log("image loaded, displayed, and dialog resized - width " + width + " height " + height);
+      $("#animal_reveal").css("display", "inline");
+      var width = $("#animal_reveal").width();
+      var height = $("#animal_reveal").height();
+      $(".vm--modal").width(width).height(height).css('top', '50px');
+      //neet to reposition the dialog? (left margin too wide)
+    }
+  },
 }
 </script>
