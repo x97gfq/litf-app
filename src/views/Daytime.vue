38337d4e8aeea9b2d2c0608eef84de0f8cccc361
Jamie Symonds <x97gfq@hotmail.com>
2020-12-18 22:15:27 -0400

    daytime (liam)

---
+++ src/views/Daytime.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home daytime">
+    <div class="bottom_nav">
+        <p>
+            <router-link to="/">Home</router-link>
+        </p>
+        <audio autoplay loop id="forest_audio">
+            <source src="@/assets/audio/daytime.wav" type="audio/ogg">
+        </audio>   
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home {
+  color: rgb(251,288,64);
+  font-family: Baskerville;
+}
+#coverimg {
+  display: none;
+  height: 100%;
+}
+
+.daytime {
+  background-image: url('~@/assets/backgrounds/daytimebackground.jpg');
+  height: 100%;
+  background-repeat: repeat-x;
+  background-size: contain;
+}
+</style>
+
+<script>
+export default {
+  name: 'Daytime'
+}
+</script>
