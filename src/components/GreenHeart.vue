8516f95db043204dc8bcffeed7e57146f73ec068
Jamie Symonds <x97gfq@hotmail.com>
2021-02-09 19:25:44 -0400

    infrastructure in place to handle resources

--- src/components/GreenHeart.vue
+++ src/components/GreenHeart.vue
@@ -1,22 +1,19 @@
 <template>
     <div class="heart">
       <p class = "popup">
-        Mental Health resource goes here
+        Resource: {{ selectedResource.name }}<br/>
+        Website: 
+        <a :href="selectedResource.website" target="new">
+          {{ selectedResource.website }}
+        </a>
       </p>
-      <p>
-        Current Latitude: {{ latitude }}<br/>
-        Current Longitude: {{ longitude }}<br/>
-      </p>
-
-      <div v-for="(resource, index) in resources" :key="resource.id">
-        {{ index }} - {{ resource.name }}
-      </div>
-
     </div>
 </template>
 
 <style scoped>
-
+a {
+  color: blue;
+}
 </style>
 
 <script>
@@ -27,16 +24,43 @@ export default {
   },
   data () {
     return {
-      resources: []
+      resources: [],
+      selectedResource: []
     }
   },
   methods: {
     getResources () {
       this.resources = this.$store.getters.getResourceList;
+    },
+    chooseResource () {
+      // need algorithm to choose nearest resource (using latitude/longitude?)
+      // if location data is available
+      //
+      //
+      //
+      // iterate over resources and print the name of each
+      this.resources.forEach(resource => {
+        console.log(resource.name)
+      });
+      //
+      //
+      //
+      // for now, use the first one in the list (position 0).
+      this.selectedResource = this.resources[0];
+      //
+      //
+      //
+      console.log(this.selectedResource);
+      console.log(this.latitude);
+      console.log(this.longitude);
+      //
+      //
+      //
     }
   },
   mounted() {
     this.getResources();
+    this.chooseResource();
   },
   computed: {
     latitude() {
