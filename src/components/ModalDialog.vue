4359605934fa571ec09fc85ea319aa3c02fef7f5
Jamie Symonds <x97gfq@hotmail.com>
2021-02-13 00:54:32 -0400

    updated animal modal

---
+++ src/components/ModalDialog.vue
@@ -0,0 +1,12 @@
+<template>
+  <teleport to="body">
+    <div
+        role="dialog"
+        ref="modal"
+        aria-modal="true"
+        aria-labelledby="modal-headline"
+    >
+       Awiwi
+    </div>
+  </teleport>
+</template>
